5001bd33fefb3759cdc9baec3ca288ead52c527b
Cam-Will <77943697+Cam-Will@users.noreply.github.com>
2021-08-19 20:30:56 -0400

    Add files via upload

---
+++ rock_paper_scissors.py
@@ -0,0 +1,10 @@
+#Build and application when both a user and computer can select either Rock, Paper or Scissors, determine the winner, and display the results
+#For reference:
+"""Rock beats Scissors
+Scissors beats Paper
+Paper beats Rock
+Part I
+Compare user input agains computer choice and determine a winner
+
+Part II
+Compare user input agains computer choice and determine a winner.  The winner will be the best 5 games, and end as soon at someone wins 3 games."""
